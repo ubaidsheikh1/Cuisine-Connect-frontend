6f070d9d2e6fce0c7adc2cb97f891132f2c1c31e
ubaidsheikh <us731506@gmail.com>
2024-04-26 10:45:29 +0530

    first commit

---
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,45 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+children: React.ReactNode;
+}
+
+const Auth0ProviderWithNavigate = ({ children}: Props)=> {
+
+      const navigate = useNavigate();
+    //   const {getAccessTokenSilently } = useAuth0();
+      const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+      const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+      const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+      const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+       if(!domain ||!clientId || !redirectUri || !audience) {
+        throw new Error("unable to initialise auth");
+       }
+
+       const onRedirectCallback = async () => {
+
+        // const token = await getAccessTokenSilently();
+        // console.log("token",token);
+        navigate("/auth-callback")
+        };
+    return (
+        <Auth0Provider
+         domain={domain}
+          clientId={clientId}
+           authorizationParams={{
+            redirect_uri: redirectUri,
+            audience,
+        }}
+        onRedirectCallback={onRedirectCallback}
+        >
+            {children}
+        </Auth0Provider>
+    )
+  
+};
+
+
+
+export default Auth0ProviderWithNavigate
